f3651989521612d39b9a86a89836aaa6e3541928
Azizka21 <sudoheckpentagon@gmail.com>
2025-05-21 20:13:53 +0500

    downloading subtitles for further work

--- content.js
+++ content.js
@@ -1,5 +1,13 @@
 chrome.runtime.onMessage.addListener(details => {
     if (details.type === "update-subtitles"){
+        fetch(details.subtitles)
+            .then(response => {
+                if (!response.ok) throw new Error('Сервер вернул ошибку');
+                return response.text()
+            })
+            .then(response => {
+                window.subtitles = response
+            })
         window.subtitles = details.subtitles
     }
     if (details.type === "log-url") {
