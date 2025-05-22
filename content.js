c710008cf4e945858bf3faccca5f3c4df73a2719
Azizka21 <sudoheckpentagon@gmail.com>
2025-05-22 20:02:29 +0500

    parsing vtt into subtitle blocks

--- content.js
+++ content.js
@@ -6,7 +6,15 @@ chrome.runtime.onMessage.addListener(details => {
                 return response.text()
             })
             .then(response => {
-                window.subtitles = response
+                const rawVtt = response.trim();
+                const blocks = rawVtt.replace(/^WEBVTT.*?\n+/, '').split(/\r?\n\r?\n/);
+                const parsedSubtitles = blocks.map(block => {
+                    const lines = block.split(/\r?\n/);
+                    const [start, end] = lines[0].split(' --> ');
+                    const text = lines.slice(1).join('\n');
+                    return { start, end, text };
+                });
+                window.subtitles = parsedSubtitles
             })
     }
     if (details.type === "log-url") {
